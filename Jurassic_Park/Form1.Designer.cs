741de01b43765d7eb419ca0fd61ac01b2de7815c
Бевз Роман Юрьевич <bevz.riu@students.dvfu.ru>
2020-04-19 14:32:48 +1000

    Добавьте файлы проекта.

---
+++ Jurassic_Park/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Jurassic_Park
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(9, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(132, 56);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "машины";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(129, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(42, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "люди";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(9, 107);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 26);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Начать";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(90, 107);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(84, 26);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Прервать";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(307, 12);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(383, 426);
+			this.richTextBox2.TabIndex = 7;
+			this.richTextBox2.Text = "";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(289, 139);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Рабочая область";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(702, 450);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.richTextBox2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
+
---
+++ Jurassic_Park/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace Jurassic_Park
+{
+	public partial class Form1 : Form
+	{
+		//static public Semaphore sem = new Semaphore(3, 3); //1-й параметр - какому числу объектов доступен семафор, 2-й - max число объектов, использующих семафор
+		//два потока для музея и сафари-парка
+		ThreadStart TH;
+		//Thread[] museum;
+		Thread[] safari;
+
+		//создадим семафор на разделяемый ресурс - машины
+		Semaphore transport;
+		//Semaphore guests; //хз ???
+
+		//кол-во машин и людей
+		int nCars;
+		int nPeople;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//public void people()
+		//{
+		//	while (nPeople > 0) //while (true)
+		//	{
+				
+		//		guests.WaitOne();
+		//		richTextBox1.Invoke((ThreadStart)delegate () { richTextBox1.AppendText(Convert.ToString(Thread.CurrentThread.Name) + "садится в машину\n"); });
+		//		transport.Release();
+		//		richTextBox1.Invoke((ThreadStart)delegate () { richTextBox1.AppendText(Thread.CurrentThread.Name + "Катается\n"); });
+		//		Thread.Sleep(1); //катается какое-то время
+		//		richTextBox1.Invoke((ThreadStart)delegate () { richTextBox1.AppendText(Thread.CurrentThread.Name + "выходит из машины\n"); });
+		//		guests.Release(); //машина освобождается
+		//		nPeople--; //кол-во людей уменьшается на единицу
+		//		Thread.Sleep(1); //поток засыпает на какое-то время
+		//	}
+		//}
+
+		public void cars()
+		{
+			while (nCars > 0) //while (true)
+			{
+				//А ГДЕ ЛОГИКА?
+				//Зачем тебе?
+				//Ладно.
+				Thread.Sleep(1000);
+				transport.WaitOne();
+
+				richTextBox2.Invoke((ThreadStart)delegate () { richTextBox2.AppendText(
+					Convert.ToString(Thread.CurrentThread.Name) + " садится в машину\n"); });
+				richTextBox2.Invoke((ThreadStart)delegate () { richTextBox2.AppendText(
+					Thread.CurrentThread.Name + " катается\n"); });
+				Random rand = new Random();
+				Thread.Sleep(rand.Next(1000, 5000));
+				nPeople--;
+				richTextBox2.Invoke((ThreadStart)delegate () { richTextBox2.AppendText(
+					Thread.CurrentThread.Name + " выходит из машины\n"); });
+				transport.Release();
+				Thread.Sleep(1000); 
+			}
+
+		}
+
+		//запускаем наш поток с людьми и машинами
+		public void button1_Click(object sender, EventArgs e)
+		{
+			nPeople = Int32.Parse(textBox1.Text);
+			nCars = Int32.Parse(textBox2.Text);
+			//museum = new Thread[nPeople];
+			safari = new Thread[nCars];
+			transport = new Semaphore(nPeople, nCars); //всего к семафору имеют доступ все люди, а максимальное ограничение определяется кол-вом машин
+
+			//for (int i = 0; i < nPeople; i++)
+			//{
+			//	TH = new ThreadStart(people);
+			//	museum[i] = new Thread(TH);
+			//	museum[i].Name = $"Человек {i}";
+			//	museum[i].Start();
+			//}
+			for (int i = 0; i < nCars; i++)
+			{
+				TH = new ThreadStart(cars);
+				safari[i] = new Thread(TH);
+				safari[i].Name =  $"Человек {i}";
+				safari[i].Start();
+			}
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			//for (int i = 0; i < nPeople; i++)
+			//{
+			//	museum[i].Abort();
+			//}
+			for (int i = 0; i < nCars; i++)
+			{
+				safari[i].Abort();
+			}
+		}
+
+	}
+}
+
